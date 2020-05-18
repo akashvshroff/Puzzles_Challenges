a5127e7c921c2ba8411649cc6d7281e0f4d1370f
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-05-19 01:00:34 +0530

    Edited some programs.

--- max_subarray.py
+++ max_subarray.py
@@ -1,11 +1,15 @@
 def max_sequence(arr):
-    max,curr=0,0
+    max, curr, max_seq, cur_seq = 0, 0, [], []
     for x in arr:
-        curr+=x
-        if curr<0:
-            curr=0
-        if curr>max:
-            max=curr
-    return max
+        curr += x
+        cur_seq.append(x)
+        if curr < 0:
+            curr = 0
+            cur_seq = []
+        if curr > max:
+            max = curr
+            max_seq = cur_seq[::]
+    return (max, max_seq)
 
-#print(max_sequence([-3,4,-4,3,2,1,-4,4,-1,5,-6,-7]))
+
+print(max_sequence([-3, 4, -4, 3, 2, 1, -4, 4, -1, 5, -6, -7]))
