95c9b8197e8d4258cf62dae31de3af6a4b176e96
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-06-17 18:21:21 +0530

    Added links

--- range_extraction.py
+++ range_extraction.py
@@ -4,6 +4,7 @@ def solution(args: list) -> str:
     accounted for.
     >>> solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20])
     '-6,-3-1,3-5,7-11,14,15,17-20'
+    See also: https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
     '''
     start = args[0]
     curr_end = None
