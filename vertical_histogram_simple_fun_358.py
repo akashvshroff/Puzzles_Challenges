ddce27970168e3a098498b69688f028b9254de1c
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-03 15:53:14 +0530

    Initial commit for a fun kata

---
+++ vertical_histogram_simple_fun_358.py
@@ -0,0 +1,8 @@
+import string
+
+
+def vertical_histogram(text):
+    """
+    Display a vertical histogram of occurences of uppercase letters in strings.
+    See also: https://www.codewars.com/kata/59cf0ba5d751dffef300001f
+    """
