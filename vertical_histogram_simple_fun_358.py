1a82660976059dc35ea787aee8aa5baa3bf73c0a
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-03 17:27:25 +0530

    Finished Kata!

--- vertical_histogram_simple_fun_358.py
+++ vertical_histogram_simple_fun_358.py
@@ -1,8 +1,20 @@
 import string
 
 
-def vertical_histogram(text):
+def vertical_histogram_of(s):
     """
     Display a vertical histogram of occurences of uppercase letters in strings.
     See also: https://www.codewars.com/kata/59cf0ba5d751dffef300001f
     """
+    letter_count = {letter: s.count(letter) for letter in string.ascii_uppercase if letter in s}
+    hist = ""
+    if not letter_count:
+        return hist
+    letters = letter_count.keys()
+    i_max = max(letter_count.values())
+    while i_max > 0:
+        row = ['*' if letter_count[letter] >= i_max else ' ' for letter in letters]
+        hist += ' '.join(row).rstrip() + '\n'
+        i_max -= 1
+    hist += ' '.join(letters).rstrip()
+    return hist
