18998f987350098830774d4b22974b8cc298719f
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-13 14:50:46 +0530

    Refactoring.

--- consec_strings.py
+++ consec_strings.py
@@ -4,16 +4,10 @@ def longest_consec(strarr, k):
     """
     if not strarr or k <= 0 or k > len(strarr):
         return ''
-    elif k == 1:
-        return max(strarr, key=len)
-    elif k == len(strarr):
-        return ''.join(strarr)
-    else:
-        curr_max = ''
-        for i in range(0, len(strarr)-k+1, 1):
-            sub_string = ''.join(strarr[i:i+k])
-            if not curr_max:
-                curr_max = sub_string
-            elif curr_max and len(sub_string) > len(curr_max):
-                curr_max = sub_string
-        return curr_max
+    max_lenght = sum(len(strarr[i]) for i in range(k))
+    max_index = 0
+    for i in range(1, n-1):
+        curr_lenght = max_lenght - strarr[i-1] + starr[i+k-1]
+        if max_length < curr_lenght:
+            max_index, max_length = i, curr_lenght
+    return ''.join(strarr[max_index:max_index+k])
