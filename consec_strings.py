99da91a40164ef9343ad7c44d3599b64c953f991
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-13 14:51:04 +0530

    Wrong commit.

--- consec_strings.py
+++ consec_strings.py
@@ -2,12 +2,16 @@ def longest_consec(strarr, k):
     """
     Longest consecutive string of terms lenght k from an array of string.
     """
-    if not strarr or k <= 0 or k > len(strarr):
+    n = len(strarr)
+    if n == 0 or k > n or k <= 0:
         return ''
-    max_lenght = sum(len(strarr[i]) for i in range(k))
+
+    length = sum(len(strarr[i]) for i in range(k))
+    max_length = length
     max_index = 0
-    for i in range(1, n-1):
-        curr_lenght = max_lenght - strarr[i-1] + starr[i+k-1]
-        if max_length < curr_lenght:
-            max_index, max_length = i, curr_lenght
-    return ''.join(strarr[max_index:max_index+k])
+    for index in range(0, n - k):
+        length += len(strarr[index + k]) - len(strarr[index])
+        if length > max_length:
+            max_length, max_index = length, index + 1
+
+    return ''.join(strarr[max_index:max_index + k])
