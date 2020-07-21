4de498f07d869444b8b62b2cff61e683e5aee5b6
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-21 15:34:44 +0530

    Another coding problem!

--- all_balanced_parenthesis.py
+++ all_balanced_parenthesis.py
@@ -1,4 +1,18 @@
-'''
-TO DO: Given n, find the number of sequences of n pairs of parentheses where
-       the parentheses are all balanced.
-'''
+def balance_parenthesis(str_para):
+    """
+    Given a string of parenthesis - (),{} or [] - Return whether it is fully
+    balanced and correctly oriented or not.
+    """
+    if not str_para:
+        return True
+    paren = {'(': ')', '{': '}', '[': ']'}
+    curr_stack = [str_para[0]]
+    for para in str_para[1:]:
+        if not curr_stack or paren[curr_stack[-1]] != para:
+            curr_stack.append(para)
+        else:
+            curr_stack.pop()
+    return not len(curr_stack)
+
+
+print(balance_parenthesis("([])[]({})"))
