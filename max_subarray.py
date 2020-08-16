f366afc32bd881d1c1672ac77c59f7e625fbac04
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-16 23:32:42 +0530

    Fixed naming.

--- max_subarray.py
+++ max_subarray.py
@@ -1,15 +1,15 @@
 def max_sequence(arr):
-    max, curr, max_seq, cur_seq = 0, 0, [], []
+    max_sum, curr, max_seq, cur_seq = 0, 0, [], []
     for x in arr:
         curr += x
         cur_seq.append(x)
         if curr < 0:
             curr = 0
             cur_seq = []
-        if curr > max:
-            max = curr
+        if curr > max_sum:
+            max_sum = curr
             max_seq = cur_seq[::]
-    return (max, max_seq)
+    return (max_sum, max_seq)
 
 
 print(max_sequence([-3, 4, -4, 3, 2, 1, -4, 4, -1, 5, -6, -7]))
