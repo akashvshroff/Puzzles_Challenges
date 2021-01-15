d0ac58402fee2c0f61e7dc5625eff9c30de7e29e
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-01-15 14:11:30 +0530

    Refactored

--- next_biggest.py
+++ next_biggest.py
@@ -1,15 +1,18 @@
 def next_bigger(n):
-    a = list(str((n)))
-    r,l,f = [],[],[]
-    for i in range(len(a)-1,0,-1):
-        if a[i]>a[i-1]:
-           p = a[i-1]
-           r = a[i:]
-           l = a[:i-1]
-           for j in range(len(r)-1,-1,-1):
-                if r[j]>p:
-                    r[j],p = p,r[j]
-                    r = sorted(r)
-                    f = l + list(p) + r
-                    return int(''.join(f))
-    return -1
+    """
+    Given an integer, find the next permutation of it in absolute order.
+    For example, given 48975, the next permutation would be 49578.
+
+    If no such number exists, example given 4321, return the number itself.
+    """
+    digits = list(str(n))
+    for i in range(len(digits) - 1, 0, -1):
+        if digits[i] > digits[i - 1]:
+            digits[i], digits[i - 1] = digits[i - 1], digits[i]
+            num = digits[:i]
+            rem = sorted(digits[i:])
+            return int(''.join(num+rem))
+    return n
+
+
+print(next_bigger(48975))
