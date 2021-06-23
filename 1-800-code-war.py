bcdee5a38049061f58c05e2010abb22376f41ae7
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-06-23 10:54:09 +0530

    A fun kata

--- 1-800-code-war.py
+++ 1-800-code-war.py
@@ -1,59 +1,34 @@
 import itertools as it
-
-
-def get_possibilities(comps, letter_dict, words_dict):
-    comp_poss = []
-    for comp in comps:
-        poss = []
-        for letter in comp:
-            poss.append(letter_dict[letter])
-        poss_words = list(it.product(*poss))
-        temp = []
-        for word_tuple in poss_words:
-            word = ''.join(word_tuple)
-            if words_dict.get(word, False):
-                temp.append(word)
-        comp_poss.append(temp)
-    comp_poss = list(it.product(*comp_poss))
-    possibilities = {'1-800-{}-{}'.format(*poss) for poss in comp_poss}
-    return possibilities
+from collections import defaultdict
 
 
 def check1800(s):
-    letter_dict = {
-        'A': 'ABC',
-        'B': 'ABC',
-        'C': 'ABC',
-        'D': 'DEF',
-        'E': 'DEF',
-        'F': 'DEF',
-        'G': 'GHI',
-        'H': 'GHI',
-        'I': 'GHI',
-        'J': 'JKL',
-        'K': 'JKL',
-        'L': 'JKL',
-        'M': 'MNO',
-        'N': 'MNO',
-        'O': 'MNO',
-        'P': 'PQRS',
-        'S': 'PQRS',
-        'Q': 'PQRS',
-        'R': 'PQRS',
-        'T': 'TUV',
-        'U': 'TUV',
-        'V': 'TUV',
-        'W': 'WXYZ',
-        'X': 'WXYZ',
-        'Y': 'WXYZ',
-        'Z': 'WXYZ'
-    }
-    words_dict = {word: True for word in WORDS}  # WORDS is a preloaded list
-    comps = s.split('-')[2:]
-    poss1 = get_possibilities(comps, letter_dict, words_dict)
-    if len(comps[0]) == 3:
-        comps = [comps[0]+comps[1][0], comps[1][1:]]
-    else:
-        comps = [comps[0][:-1], comps[0][-1]+comps[1]]
-    poss2 = get_possibilities(comps, letter_dict, words_dict)
-    return poss1.union(poss2)
+    """
+    Given the 1-800 (phone word) number that the company wants to use,
+    you need to check against all known words and return the set of all possible 1-800 numbers
+    which can be formed using those same digits.
+
+    There is a preloaded array of lots of 3 and 4 letter (upper-case) words:
+    For Python it is: WORDS
+
+    Link: https://www.codewars.com/kata/5a3267b2ee1aaead3d000037/train/python
+    """
+    num_translation = str.maketrans(
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '22233344455566677778889999')
+    nums_dict = defaultdict(list)
+    for word in WORDS:
+        nums_dict[word.translate(num_translation)].append(word)
+
+    number = s[6:].replace('-', '').translate(num_translation)
+
+    # we take the cartesian product of all the options for the first word and all the options for the second.
+
+    # case 1, first word is 3 letter & second is 4 letter
+    possibilities1 = {'1-800-{}-{}'.format(*poss) for poss in it.product(
+        nums_dict[number[:3]], nums_dict[number[3:]])}
+
+    # case 2, first word is 4 letter & second is 3 letter
+    possibilities2 = {'1-800-{}-{}'.format(*poss) for poss in it.product(
+        nums_dict[number[:4]], nums_dict[number[4:]])}
+
+    return possibilities1.union(possibilities2)
